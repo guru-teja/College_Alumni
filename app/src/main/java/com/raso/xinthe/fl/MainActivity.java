85ac7199c73e91e90d0629e95b2df53b2091e4ca
Guru Teja <guruteja@gmail.com>
2016-11-11 11:35:04 +0530

    First Andriod Commit to Github

---
+++ app/src/main/java/com/raso/xinthe/fl/MainActivity.java
@@ -0,0 +1,45 @@
+package com.raso.xinthe.fl;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        R.layout.activity_main.getForeground().setAlpha( 220);
+//        layout_MainMenu = (RelativeLayout) findViewById(R.id.activity_main);
+//        layout_MainMenu.getForeground().setAlpha( 0);
+
+      //  R.layout.activity_main.getBackground().setAlpha(100);
+
+        //R.layout.activity_main.setAlpha(120);
+      //  RelativeLayout Rl = (RelativeLayout) findViewById(R.id.back);
+
+
+//         Bitmap bmp;
+//         ImageView img;
+//        img = (ImageView)findViewById(R.i);
+//        BitmapDrawable abmp = (BitmapDrawable)img.getDrawable();
+
+//            Rl.setBackgroundResource(R.drawable.bg);
+//
+//
+//
+//        Drawable drawable = this.getResources().getDrawable(R.drawable.bg);
+//        drawable.setAlpha(200);
+//       Rl.setBackground(drawable);//  drawable.setAlpha(200);
+
+
+        }
+
+    }
+
